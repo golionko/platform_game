ebe9cca9c0d5c30413e0210e36b2c4ae1f31636d
golionko <golionko@gmail.com>
2019-09-27 00:30:35 -0400

    cleanup

--- modules.js
+++
@@ -1,6 +0,0 @@
-import Level from './js/Level.js';
-import State from './js/State.js';
-import Vec from './js/Vec.js';
-import Player from './js/Player.js';
-import Lava from './js/Lava.js';
-import Coin from './js/Coin.js';
